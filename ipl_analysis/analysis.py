6403be2861232ce36aa454c934b61c4fb5049c57
subhash singh <subhashrs1997@gmail.com>
2023-09-02 18:23:29 +0530

    batsman analysis

--- ipl_analysis/analysis.py
+++ ipl_analysis/analysis.py
@@ -7,10 +7,11 @@ def file_open(path):
     except Exception as e:
         print(e)
 
-df1 = file_open(r"D:\imp\Projects\ipl_analysis\ipl_datasets\ipl1.csv")
-df2 = file_open(r"D:\imp\Projects\ipl_analysis\ipl_datasets\ipl1.csv")
-
+df1 = pd.read_csv(r"D:\imp\Projects\ipl_analysis\ipl_datasets\ipl1.csv",usecols=["ID","ballnumber","batter","bowler","batsman_run","isWicketDelivery","player_out","kind","BattingTeam"])
+df2 = pd.read_csv(r"D:\imp\Projects\ipl_analysis\ipl_datasets\ipl2.csv",usecols = ["ID","City","Season","MatchNumber","Team1","Team2","WinningTeam","Player_of_Match"])
 batter = df1["batter"].unique().tolist()
+bowler = df1["bowler"].unique().tolist()
+
 
 # batter's performance analysis
 def batsman_analysis(batter):
@@ -30,3 +31,59 @@ def batsman_analysis(batter):
     data = {"Match": match, "Runs":batsman_runs,"HS": highest_score,"Avg": avg_score,"Strike_rate":strike_rate,"100": hundreds,"50": fifties,"4s": fours,"6s":sixes}
     batsman_df = pd.DataFrame(data, index = [batter])
     return batsman_df
+
+def batsman_overall(batter):
+    df1[["ID","batter","batsman_run","BattingTeam"]]
+
+    final_df = df1.merge(df2, on = "ID")[["ID","batter","batsman_run","Team1","Team2","BattingTeam"]]
+    new_df = final_df[final_df["batter"] == batter]
+
+    batsman_team = new_df["BattingTeam"].values[0]
+    yb = new_df[new_df["Team1"] == batsman_team].loc[:,["Team1","Team2"]]
+    tm1 = yb.shift(-1,axis = 1)["Team1"].values
+    yb1 = new_df[new_df["Team2"] == "Rajasthan Royals"].loc[:,["Team1","Team2"]]
+    tm2 = yb1.shift(1,axis = 1)["Team2"].values
+    new_df.loc[yb.index,["Team1"]] = tm1
+    new_df.loc[yb1.index,["Team2"]] = tm2
+
+    overall = new_df.groupby(["batter","Team1"])["batsman_run"].sum().sort_values(ascending = False).reset_index()
+    balls = new_df.groupby(["batter","Team1"])["batsman_run"].count().reset_index().rename(columns = {"batsman_run":"balls"})
+
+    four_df = new_df[(new_df["batsman_run"] == 4)]
+    fours = four_df.groupby(["Team1"])["batsman_run"].count().reset_index().rename(columns = {"batsman_run":"4s"})
+    six_df = new_df[(new_df["batsman_run"] == 6)]
+    sixes = six_df.groupby(["Team1"])["batsman_run"].count().reset_index().rename(columns = {"batsman_run":"6s"})
+    six_fours = sixes.merge(fours, on = "Team1")
+
+    hundred_df = new_df.groupby(["ID","Team1"])["batsman_run"].sum().reset_index()
+    hundreds = hundred_df[hundred_df["batsman_run"] >= 100].groupby(["Team1"])["batsman_run"].count().reset_index()
+    fifties = hundred_df[(hundred_df["batsman_run"] >= 50) & (hundred_df["batsman_run"] < 100)].groupby(["Team1"])["batsman_run"].count().reset_index()
+    fifties.rename(columns = {"batsman_run": "50"}, inplace = True)
+    hundreds.rename(columns = {"batsman_run": "100"},inplace = True)
+    hundred_fifties = fifties.merge(hundreds, on = "Team1", how = "left").fillna(0)
+
+    matches = new_df.groupby(["ID","Team1"])["batter"].count().reset_index()["Team1"].value_counts().reset_index().rename(columns = {"index":"Team1","Team1":"Match"})
+    max_score = new_df.groupby(["ID","Team1"])["batsman_run"].sum().reset_index().groupby("Team1")["batsman_run"].max().reset_index()
+    max_score.rename(columns= {"batsman_run":"HS"}, inplace = True)
+
+    overall = overall.merge(matches, on = "Team1").merge(max_score, on = "Team1").merge(balls, on = ["batter","Team1"])
+    overall = overall.merge(hundred_fifties, on = "Team1", how = "left").merge(six_fours, on = "Team1", how = "left")
+
+    overall["strike_rate"] =  round((overall["batsman_run"]/overall["balls"])*100,2)
+    overall["average"] =  round(overall["batsman_run"]/overall["Match"],2)
+    overall.fillna(0,inplace = True)
+
+    overall["50"] = overall["50"].astype("int")
+    overall["100"] = overall["100"].astype("int")
+    overall["6s"] = overall["6s"].astype("int")
+    overall["4s"] = overall["4s"].astype("int")
+    overall.drop(columns= ["batter","balls"], inplace = True)
+    overall.rename(columns= {"Team1":"Teams"}, inplace = True)
+    overall.rename(columns= {"batsman_run":"Runs"}, inplace = True)
+    overall.set_index("Teams", inplace = True)
+
+    overall = overall.iloc[:,[1,0,2,8,7,3,4,6,5]]
+    return overall
+
+def bowler_analysis(bowler):
+    pass
--- ipl_analysis/app.py
+++ ipl_analysis/app.py
@@ -1,15 +1,20 @@
 import streamlit as st
 import pandas as pd
-import pandas as pd
+import numpy as np
 import analysis as alt
+import matplotlib.pyplot as plt
 
+st.set_page_config(layout= "wide", page_title= "ipl analysis")
 st.sidebar.title("Ipl Analysis")
 check_box = st.sidebar.selectbox("Select One",["batter","bowler","team"])
 
+unique_batter = alt.batter
+unique_bowler = alt.bowler
+
 if check_box == "batter":
     st.sidebar.subheader("Batsman name")
-    selected_batsman = st.sidebar.selectbox("Select Batsman",alt.batter)
-    btn1 = st.sidebar.button("View batsman record")
+    selected_batsman = st.sidebar.selectbox("Select Batsman",unique_batter)
+    btn1 = st.sidebar.button("Batsman's Record")
 
     if btn1:
         st.header(f"{selected_batsman}")
@@ -18,4 +23,40 @@ if check_box == "batter":
         st.dataframe(batsman_record)
 
         st.subheader(f"{selected_batsman}'s record against team")
+        batsman_overall = alt.batsman_overall(selected_batsman)
+        st.dataframe(batsman_overall)
 
+        
+        fig,axs = plt.subplots(4,1, layout = "constrained",figsize = (8,16))
+        axs[0].bar(data = batsman_overall,x = batsman_overall.index,height = "Runs")
+        axs[0].set_xticklabels(batsman_overall.index,rotation = -20)
+        # axs[0].set_title(f"{selected_batsman}'s runs", fontdict= {"fontsize": 18,"color": "red"})
+        axs[0].set_ylabel("Total runs", fontdict= {"fontsize": 14,"color": "red"})
+        axs[0].tick_params(axis= "both",which = "both", labelsize = 8)
+        
+        axs[1].bar(data = batsman_overall, x = batsman_overall.index,height = "average")
+        axs[1].set_xticklabels(batsman_overall.index,rotation = -20)
+        axs[1].set_ylabel("Average", fontdict= {"fontsize": 14,"color": "red"})
+        axs[1].tick_params(axis= "both",which = "both", labelsize = 8)
+
+        axs[2].bar(data = batsman_overall,x = batsman_overall.index,height = "strike_rate")
+        axs[2].set_xticklabels(batsman_overall.index,rotation = -20)
+        axs[2].set_ylabel("Strik rate", fontdict= {"fontsize": 14,"color": "red"})
+        axs[2].tick_params(axis= "both",which = "both", labelsize = 8)
+
+        axs[3].bar(data = batsman_overall,x = batsman_overall.index,height = "HS")
+        axs[3].set_xticklabels(batsman_overall.index,rotation = -20)
+        axs[3].set_ylabel("Highest score", fontdict= {"fontsize": 14,"color": "red"})
+        axs[3].tick_params(axis= "both",which = "both", labelsize = 8)
+        st.pyplot(fig)
+
+elif check_box == "bowler":
+    st.sidebar.subheader("Bowler name")
+    select_bowler = st.sidebar.selectbox("Select Bowler", unique_bowler)
+    btn2 = st.sidebar.button("Bowler's Record")
+
+    if btn2:
+        st.header(f"{select_bowler}")
+        st.subheader(f"{select_bowler}'s overall record")
+        
+        
